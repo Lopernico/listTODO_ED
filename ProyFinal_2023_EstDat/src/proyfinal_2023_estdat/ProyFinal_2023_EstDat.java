56c567e9b683bf7e385f70a8ff0b7add1cbfcfb1
Luis Fernando Aguilar Ortiz <79117692+Lopernico@users.noreply.github.com>
2023-11-25 13:32:18 -0700

    proy base

---
+++ ProyFinal_2023_EstDat/src/proyfinal_2023_estdat/ProyFinal_2023_EstDat.java
@@ -0,0 +1,21 @@
+/*
+ * Luis Fernando Aguilar Ortiz 
+ITSON ISW 228419
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyfinal_2023_estdat;
+
+/**
+ *
+ * @author Luis Fernando Aguilar Ortiz 228419 ITSON ISW obregon-Camp-Nainari
+ */
+public class ProyFinal_2023_EstDat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
